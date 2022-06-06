bb2b2806fb1b72fd49a4f8a678e3b9d93c8b75d5
Kyle Bennett <kyle@slik.com.au>
2022-06-06 15:54:15 +1000

    removed GA

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,8 @@
-import ReactGA from 'react-ga'
+// import ReactGA from 'react-ga'
 import { triggerFloodlightPageView } from './src/utils/floodlights'
 import './src/assets/css/main.scss'
 import 'intersection-observer'
 
-ReactGA.initialize(process.env.GA_ID)
-export const onRouteUpdate = state => ReactGA.pageview(state.location.pathname)
+// ReactGA.initialize(process.env.GA_ID)
+// export const onRouteUpdate = state => ReactGA.pageview(state.location.pathname)
 triggerFloodlightPageView()
-
-
